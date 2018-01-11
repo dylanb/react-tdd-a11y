49a1f4e6281804567d0b6f0c51ce7ff8b3392388
Dylan Barrell <dylan@barrell.com>
2018-01-10 21:27:05 -0500

    test: Add integration test for accessibility

--- test/integration.js
+++ test/integration.js
@@ -1,5 +1,5 @@
 const Selenium = require('selenium-webdriver'),
-	  	AxeBuilder = require('axe-webdriverjs')
+    AxeBuilder = require('axe-webdriverjs')
 const assert = require('assert')
 
 const TestUtils = require('./utils.js')
@@ -7,32 +7,43 @@ const TestUtils = require('./utils.js')
 var port = process.env.npm_package_config_port || 3006
 
 describe('Integration tests', function() {
-	this.timeout(10000)
+  this.timeout(10000)
 
-	let driver
+  let driver
 
-	beforeEach(function(done) {
-		driver = new Selenium.Builder()
-		  .forBrowser('chrome')
-		  .build()
-		driver
-			.get(`http://localhost:${port}`)
-		  .then(function() {
-  			done()
-		  })
-	})
+  beforeEach(function(done) {
+    driver = new Selenium.Builder()
+      .forBrowser('chrome')
+      .build()
+    driver
+      .get(`http://localhost:${port}`)
+      .then(function() {
+      done()
+      })
+  })
 
-	afterEach(function(done) {
-		driver.quit().then(function() {
-			done()
-		})
-	})
+  afterEach(function(done) {
+    driver.quit().then(function() {
+      done()
+    })
+  })
 
-	it('should find the App element', function(done) {
-		driver
-			.findElement(Selenium.By.css('.App'))
-		  .then(function() {
-	        done()
-		  })
-	})
+  it('should find the App element', function(done) {
+    driver
+      .findElement(Selenium.By.css('.App'))
+      .then(function() {
+      done()
+      })
+  })
+  it('should pass the accessibility tests', function (done) {
+    driver.findElement(Selenium.By.css('.App'))
+      .then(function() {
+        new AxeBuilder(driver)
+          .analyze(function (results) {
+            TestUtils.printViolations(results.violations)
+            assert.equal(results.violations.length, 0)
+            done()
+          })
+      })
+  })
 })
