49ccaeeffb2d161bbf3dc196778eb483d1f9380a
Dylan Barrell <dylan@barrell.com>
2018-01-10 23:11:06 -0500

    test: add accessibility tests for required fields

--- src/login.js
+++ src/login.js
@@ -3,18 +3,18 @@ import React, { Component } from 'react'
 class Login extends Component {
   render() {
   return (
-    <div className="login">
+    <form className="login">
       <h2>Login</h2>
       <div className="field">
-        <label for="email">Email *</label>
-        <input type="text" name="email" id="email"/>
+        <label htmlFor="email">Email *</label>
+        <input type="text" name="email" id="email" required/>
       </div>
       <div className="field">
-        <label className="field" for="password">Password *</label>
-        <input type="password" name="password" id="password"/>
+        <label className="field" htmlFor="password">Password *</label>
+        <input type="password" name="password" id="password" required/>
       </div>
       <button className="submit-btn">Submit</button>
-    </div>
+    </form>
   )
   }
 }
--- test/integration.js
+++ test/integration.js
@@ -6,32 +6,32 @@ const TestUtils = require('./utils.js')
 
 var port = process.env.npm_package_config_port || 3006
 
-describe('Integration tests', function() {
+describe('Integration tests', function () {
   this.timeout(10000)
 
   let driver
 
-  beforeEach(function(done) {
+  beforeEach(function (done) {
     driver = new Selenium.Builder()
       .forBrowser('chrome')
       .build()
     driver
       .get(`http://localhost:${port}`)
-      .then(function() {
-      done()
+      .then(function () {
+        done()
       })
   })
 
-  afterEach(function(done) {
-    driver.quit().then(function() {
+  afterEach(function (done) {
+    driver.quit().then(function () {
       done()
     })
   })
 
-  it('should find the App element', function(done) {
+  it('should find the App element', function (done) {
     driver
       .findElement(Selenium.By.css('.App'))
-      .then(function() {
+      .then(function () {
         new AxeBuilder(driver)
           .analyze(function (results) {
             TestUtils.printViolations(results.violations)
@@ -43,7 +43,7 @@ describe('Integration tests', function() {
   it('should have a link pointing to the "login" route', function (done) {
     driver
       .findElement(Selenium.By.css('.App a[href="/login"]'))
-      .then(function() {
+      .then(function () {
         new AxeBuilder(driver)
           .analyze(function (results) {
             TestUtils.printViolations(results.violations)
@@ -51,5 +51,22 @@ describe('Integration tests', function() {
             done()
           })
       })
+  })
+  describe('the login page', function () {
+    beforeEach(function (done) {
+      driver
+        .get(`http://localhost:${port}/login`)
+        .then(function() {
+          done()
+        })
+    })
+    it('should have two form fields that are required', function (done) {
+          driver.findElement(Selenium.By.css('.App form'))
+            .then(function (form) {
+              assert.ok(form.findElement(Selenium.By.css('#email[required]')))
+              assert.ok(form.findElement(Selenium.By.css('#password[required]')))
+              done()
+            })
+    })
   })
 })
