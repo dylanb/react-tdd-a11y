64f4ac0abfe90362530cd78cc278cf1c33389de3
Dylan Barrell <dylan@barrell.com>
2018-01-10 21:20:38 -0500

    style: remove semi-colons from the integration tests

--- test/integration.js
+++ test/integration.js
@@ -1,38 +1,38 @@
 const Selenium = require('selenium-webdriver'),
-	  	AxeBuilder = require('axe-webdriverjs');
-const assert = require('assert');
+	  	AxeBuilder = require('axe-webdriverjs')
+const assert = require('assert')
 
-const TestUtils = require('./utils.js');
+const TestUtils = require('./utils.js')
 
-var port = process.env.npm_package_config_port || 3006;
+var port = process.env.npm_package_config_port || 3006
 
 describe('Integration tests', function() {
-	this.timeout(10000);
+	this.timeout(10000)
 
-	let driver;
+	let driver
 
 	beforeEach(function(done) {
 		driver = new Selenium.Builder()
 		  .forBrowser('chrome')
-		  .build();
+		  .build()
 		driver
 			.get(`http://localhost:${port}`)
 		  .then(function() {
-  			done();
+  			done()
 		  })
-	});
+	})
 
 	afterEach(function(done) {
 		driver.quit().then(function() {
-			done();
-		});
-	});
+			done()
+		})
+	})
 
 	it('should find the App element', function(done) {
 		driver
 			.findElement(Selenium.By.css('.App'))
 		  .then(function() {
-	        done();
-		  });
-	});
-});
+	        done()
+		  })
+	})
+})
